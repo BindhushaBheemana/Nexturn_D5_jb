59b50bdbf9880c2db5994423282f242babef855e
BindhushaBheemana <125570277+BindhushaBheemana@users.noreply.github.com>
2025-03-05 15:40:24 +0530

    Add files via upload

---
+++ daay5.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+public class daay5 {
+    public static void main(String[] args) {
+       //day5 j=new day5();
+        //System.out.println(j.add(5,6));
+        
+        Scanner sc=new Scanner(System.in);
+        /*int a=sc.nextInt();
+        int b=sc.nextInt();
+        max(a, b);*/
+        /*String name="hglah";
+        String company3="hjkk";
+        sendMail(name,company3);*/
+
+        int num=sc.nextInt();
+        System.out.println("hello");
+      //  System.out.println(fact(num));
+      //System.out.println(fact1(num));
+      /* for (int i = 0; i < num; i++) {
+        System.out.print(fib(i) + " ");
+    } */
+    //System.out.println(sumDig(num));
+
+    }
+    /*public  int add(int a,int b){
+        int c=a+b;
+        return c;
+    }*/
+    
+    /*public static void max(int a,int b){
+        if(a>b){
+            System.out.println("MAX:"+a);
+        }else{
+            System.out.println("MAX:"+b);
+        }
+    }*/
+
+    /*public static void sendMail(String name, String company) {
+        System.out.println("dear "+ name+" welcome "+company);
+    }*/
+  
+   /* public static int fact(int num){
+        if (num<=1) {
+            return 1;
+        }
+        return num*fact(num-1);
+    }*/
+
+   /*  public static int fact1(int num){
+        int res=1;
+        for(int i=1;i<=num;i++){
+          res=res*i;
+        }
+        return res;
+    }*/
+   /*  public static int fib(int num){
+        if (num<=1) {
+            return num;
+        }else{
+            return fib(num-1)+fib(num-2);
+        }
+    }*/
+
+    /*public static int sumDig(int num){
+        if (num==0) {
+            return 0;
+        }else{
+            return num%10+sumDig(num/10);
+        }
+    }*/
+
+   
+
+}
+
+
+
